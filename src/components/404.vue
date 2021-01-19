0098bb900ea5c5c9263a78f726691add9df5b48e
shi_yi <shi_yidashi@163.com>
2021-01-19 17:53:23 +0800

    修改404页面

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>404</div>
+  <div>
+    <div class="pic-404">
+      <el-row>
+        <img class="pic-404__parent" src="@/assets/404.png" alt="404" />
+      </el-row>
+      <el-row>
+        <router-link to="/">
+          <el-button type="primary" round style="margin-top: 50px"
+            >回到主页</el-button
+          >
+        </router-link>
+      </el-row>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +20,8 @@
 </script>
 
 <style scoped lang="scss">
+.pic-404 {
+  text-align: center;
+  padding-top: 80px;
+}
 </style>
